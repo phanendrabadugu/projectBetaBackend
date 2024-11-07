09699ab92d3575eea5735748879853a5bdaad07f
phanendrabadugu <79144761+phanendrabadugu@users.noreply.github.com>
2024-11-07 01:34:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const jobSearchRoutes = require('./routes/jobSearchRoutes');
 
 const app = express();
 
-app.use(cors())
+app.use(cors({ origin: 'https://ffdgvk.netlify.app' })); // Replace with your Netlify URL
 app.use(express.json());
 
 app.get('/', (req, res) => {
